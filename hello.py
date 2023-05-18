a1c5736563bcd9e7832300a29fb70baad133c022
Ben Koester <ben.koester@gmail.com>
2023-05-18 00:43:41 +0000

    adding azure version

--- hello.py
+++ hello.py
@@ -3,4 +3,9 @@ def add(x, y):
 
 
 result = add(1, 2)
-print(f"this is the sum: 1,2, {result}")
+#this print is OK in python 3.6 and above. It was not when the 
+#coursera video was made and azure supported 3.5 and below.
+#anyhow, how, we have both print statements in here because we were following along.
+
+#print(f"this is the sum: 1,2, {result}")
+print("This is the sum: 1,2 %s", result)
